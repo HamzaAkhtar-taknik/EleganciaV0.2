8eeb914f2259c7f7236a9052f6bb0b863b061504
Hamza Akhtar <hamzza.akhtar@taknik.com>
2021-12-30 17:10:57 -0800

    initial commit

---
+++ StaticDB/ProductsDB.js
@@ -0,0 +1,300 @@
+var  BlackBeautyv1 ="=./assets/BlackBeautyv1.png";
+var  BlackBeautyv2 ="=./assets/BlackBeautyv2.png";
+var  BlackBeautyv3 ="=./assets/BlackBeautyv3.png";
+var  DiningChairv1 ="=./assets/DiningChairv1.png";
+var  DiningChairv2 ="=./assets/DiningChairv2.png";
+var  DiningChairv3 ="=./assets/DiningChairv3.png";
+var  Elegantv1 ="=./assets/Elegantv1.png";
+var  Elegantv2 ="=./assets/Elegantv2.png";
+var  Elegantv3 ="=./assets/Elegantv3.png";
+var  Elegantv4 ="=./assets/Elegantv4.png";
+var  HighTablev1 ="=./assets/HighTablev1.png";
+var  HighTablev2 ="=./assets/HighTablev2.png";
+var  HighTablev3 ="=./assets/HighTablev3.png";
+var  LegRestv1 ="=./assets/LegRestv1.png";
+var  LegRestv2 ="=./assets/LegRestv2.png";
+var  LegRestv3 ="=./assets/LegRestv3.png";
+var  MidCenturyv1 ="=./assets/MidCenturyv1.png";
+var  MidCenturyv2 ="=./assets/MidCenturyv2.png";
+var  MidCenturyv3 ="=./assets/MidCenturyv3.png";
+var  RoyalSofav1 ="=./assets/RoyalSofav1.png";
+var  RoyalSofav2 ="=./assets/RoyalSofav2.png";
+var  RoyalSofav3 ="=./assets/RoyalSofav3.png";
+var  SlimSofav1 ="=./assets/SlimSofav1.png";
+var  SlimSofav2 ="=./assets/SlimSofav2.png";
+var  SlimSofav3 ="=./assets/SlimSofav3.png";
+var  ThreeSeaterv1 ="=./assets/ThreeSeaterv1.png";
+var  ThreeSeaterv2 ="=./assets/ThreeSeaterv2.png";
+var  ThreeSeaterv3 ="=./assets/ThreeSeaterv3.png";
+var  Tuxedov1 ="=./assets/Tuxedov1.png";
+var  Tuxedov2 ="=./assets/Tuxedov2.png";
+var  Tuxedov3 ="=./assets/Tuxedov3.png";
+var  WingChairv1 ="=./assets/WingChairv1.png";
+var  WingChairv2 ="=./assets/WingChairv2.png";
+var  WingChairv3 ="=./assets/WingChairv3.png";
+
+var products = [
+  {
+    id: 1,
+    name: "Elegant",
+    image: [Elegantv1, Elegantv2, Elegantv3, Elegantv4],
+    price: "$ 1,598",
+    about: `Standing on solid wood legs,
+       our goes-with-anything Baltimore Dining Chairs take a page =
+       clean-lined mid-century modern designs. Its seat is upholstered in durable,
+        family-friendly fabric, making this a perfect piece for parties or family dinners alike.`,
+    dimensions:
+      `"Overall: 29.9""w x 29.75""d x 30.9""h` +
+      "\r\n" +
+      `Seat: 
+      Width: 21.6""  Depth: 22.75""  Height: 17.1"".
+      Arm height: 22"".
+      Diagonal depth: 27.4"".
+      Leg 
+      Height (front): 11.25""  Height (back): 10.9"".
+      Packaged weight: 46.4 lbs."
+    `,
+    details: `Solid rubberwood legs; engineered wood seat and back.
+    Covered in a Walnut-finished stain.
+    All wood is kiln-dried for added durability.
+    Plastic foot caps on legs help protect floors.
+    Sold in sets.
+    Made in Vietnam`,
+  },
+  {
+    id: 2,
+    name: "Wing Chair",
+    image: [WingChairv1, WingChairv2, WingChairv3],
+    price: "$ 1,998",
+    about: `The height of fashion, our mid-century Italian-inspired Finley Lounge Chair features winged arms, angled iron toothpick legs and a slim, rounded seat cushion for sleek comfort. It's an eye-catching piece for the living room or reading nook.`,
+    dimensions: `Overall: 29.9"w x 29.75"d x 30.9"h.
+    Seat: 
+    Width: 21.6"  Depth: 22.75"  Height: 17.1".
+    Arm height: 22".
+    Diagonal depth: 27.4".
+    Leg 
+    Height (front): 11.25"  Height (back): 10.9".
+    Packaged weight: 46.4 lbs.`,
+    details: `Kiln-dried engineered wood frame.
+    Metal legs with a powder-coated finish in Burnished Bronze.
+    Semi-attached, non-reversible seat cushion.
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential. 
+    var ed.`,
+  },
+  {
+    id: 3,
+    name: "Mid Century",
+    image: [MidCenturyv1, MidCenturyv2, MidCenturyv3],
+    price: "$ 1,998",
+    about: `The height of fashion, our mid-century Italian-inspired Finley Lounge Chair features winged arms, angled iron toothpick legs and a slim, rounded seat cushion for sleek comfort. It's an eye-catching piece for the living room or reading nook.`,
+    dimensions: `Overall: 19.9"w x 21"d x 37.5"h.
+    Seat height: 26".
+    Back height: 13".
+    Clearance: 19".
+    Weight capacity: 275 lbs.
+    Packaged weight: 32 lbs.`,
+    details: `Solid wood or stainless steel legs.
+    Padded, upholstered seat.
+    Swivel base.
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential.
+    Made in China.`,
+  },
+  {
+    id: 4,
+    name: "Three Seater",
+    image: [ThreeSeaterv1, ThreeSeaterv2, ThreeSeaterv3],
+    price: "$ 1,998",
+    about: `It's defined by a low-slung frame and slender profile.
+    Boomerang legs are Danish modern inspired.
+    All wood is kiln-dried for added durability.
+    Available in your choice of leathers.`,
+    dimensions: `74.4"w x 35"d x 30.7"h.
+    Interior seat width: 68.9".
+    Seat: 
+    Depth: 24" Height: 17".
+    Back height: 26.75".
+    Clearance: 9".
+    Comfortably seats: 2-3.
+    Packaged weight: 135.6 lbs.`,
+    details: `Solid pine and engineered hardwood frame with reinforced joinery.
+    All wood is kiln dried for added durability.
+    Available in your choice of genuine top-grain leather or animal-friendly vegan leather.
+    Solid oak boomerang legs (removable) in a Walnut finish.
+    Webbed seat and back support.
+    Seat cushions have fiber-wrapped, high-resiliency polyurethane foam cores.
+    Seat firmness: Medium. On a scale =1 to 5 (5 being firmest), it's a 4.
+    Back cushions are 70% poly fiber, 30% duck feather blend in down proof ticking.
+    Loose cushions with zip-off covers (non-reversible).
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential.
+    Handcrafted.
+    Made in Vietnam.`,
+  },
+  {
+    id: 5,
+    name: "Royal Sofa",
+    image: [RoyalSofav1, RoyalSofav2, RoyalSofav3],
+    price: "$ 1,998",
+    about: `The inviting silhouette wraps around you.
+    Its cloud-like seat cushion offers supreme comfort.
+    Covered in your choice of popular fabrics.
+    Contract Grade for unmatched durability.`,
+    dimensions: `Overall: 29.5"w x 31"d x 29.5"h.
+    Interior seat width: 21.5".
+    Seat 
+    Depth: 23.5"  Height: 19".
+    Arm height: 25.5".
+    Diagonal depth: 29".
+    Packaged weight: 63.6 lbs.`,
+    details: `Kiln-dried pine and engineered wood frame with reinforced joinery.
+    Webbed cushion support.
+    High-resiliency polyurethane foam that is fiber-wrapped for extra comfort.
+    Seat firmness: Medium. On a scale =1 to 5 (5 being firmest), it's a 3.
+    360-degree swivel base.
+    Loose, reversible seat cushion with zip-off cover.
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential. See more.
+    Stocked products are made in China; Made to order products are made in Mexico.`,
+  },
+  {
+    id: 6,
+    name: "Tuxedo",
+    image: [Tuxedov1, Tuxedov2, Tuxedov3],
+    price: "$ 1,998",
+    about: `Its mix of sleek frame & plush cushioning.
+    Covered in your choice of beautiful leather.
+    The down-wrapped back cushions.
+    Contract Grade for unmatched durability.`,
+    dimensions: `70"w x 35.8"d x 31.5"h.
+    Seat:
+    Width: 62" Depth: 23.2" Height: 18.5".
+    Back height: 31.5".
+    Arm height: 26".
+    Diagonal depth: 32.3".
+    Leg height: 6.7".
+    Comfortably seats: 2.`,
+    details: `Solid and engineered hardwood frame with reinforced joinery.
+    All wood is kiln dried for added durability.
+    Available in genuine top-grain leather or animal-friendly vegan leather.
+    Webbed seat and back support.
+    Seat cushions have fiber-wrapped, high-resiliency polyurethane foam cores.
+    Seat firmness: Medium. On a scale =1 to 5 (5 being firmest), it's a 3.
+    Back cushions are 70% poly fiber, 30% duck feather blend in down proof ticking (Vegan leather options have polyester only filled back cushions).
+    Loose, non-reversible cushions with zip-off covers.
+    Solid wood legs (removable).
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential
+    Genuine leather options are made in Vietnam; Vegan leather options are assembled in the USA.`,
+  },
+  {
+    id: 7,
+    name: "Leg Rest Chair",
+    image: [LegRestv1, LegRestv2, LegRestv3],
+    price: "$ 1,998",
+    about: `Inspired by the cool, clean lines of Scandinavian design, the Playa Collection features a slim profile and modern finish. Made with FSC®-certified wood and water-resistant cushions, this lounge-ready set is the sink-right-in seating you'll want for your next outdoor gathering.`,
+    dimensions: `Overall product dimensions: 36.25"w x 29.4"d x 31.2"h.
+    Interior seat width: 26.9".
+    Seat depth: 24.25".
+    Seat height: 10.9" (without cushion); 15.6" (with cushion).
+    Back height: 28".
+    Arm height: 22.7".
+    Leg height: 7.4".`,
+    details: `For set pictured, select 1 Playa Lounge Chair and 1 Playa Ottoman.
+    Components are priced individually.
+    Solid eucalyptus and mahogany wood frame.
+    Made with kiln-dried, FSC®-certified wood =responsibly managed forests.
+    Covered in a water-based Mast finish.
+    Water-resistant cushions with 100% polyester covers in Cement (included).
+    Cushions feature polyurethane foam and polyester filling.
+    Non-reversible cushions with zip-off covers.
+    Made in Indonesia.`,
+  },
+  {
+    id: 8,
+    name: "Dining Chair",
+    image: [DiningChairv1, DiningChairv2, DiningChairv3],
+    price: "$ 1,998",
+    about: `On top of its tapered legs and mid-century-inspired frame, we gave our Classic Café Dining Chair a comfortable oval back and an upholstered seat, for a chair that’s extra inviting.`,
+    dimensions: `17.3"w x 20"d x 32"h.
+    Seat: 
+    Depth: 15.7"   Height: 17.75".
+    Weight capacity: 275 lbs.`,
+    details: `Solid rubberwood frame.
+    Upholstered seat.
+    Walnut finish.
+    Plastic caps on legs help protect floors.
+    Set of 2.
+    var ed.`,
+  },
+
+  {
+    id: 9,
+    name: "Slim Sofa",
+    image: [SlimSofav1, SlimSofav2, SlimSofav3],
+    price: "$ 1,998",
+    about: `It's a grown-up addition to nurseries & playrooms.
+    The durable, hand-built wood frame.
+    Covered in your choice of beautiful leather.`,
+    dimensions: `Overall: 29"w x 37.5"d x 40.5"h.
+    Interior seat width: 24".
+    Seat 
+    Depth: 23.25"  Height: 18".
+    Back height: 40.5".
+    Required wall distance: 22".
+    Diagonal depth: 36".
+    Clearance: 4.5".
+    Packaged weight: 100 lbs.`,
+    details: `Available in your choice of genuine top-grain leather or animal-friendly vegan leather.
+    Solid ash wood frame in a Pecan (stocked) or Walnut (made to order) finish.
+    Fiber-wrapped foam cushioning.
+    Hidden steel push-back mechanism.
+    Made in China.`,
+  },
+  {
+    id: 10,
+    name: "Black Beauty",
+    image: [BlackBeautyv1, BlackBeautyv2, BlackBeautyv3],
+    price: "$ 1,998",
+    about: `The sculptural arms & wide, welcoming seat.
+    Covered in your choice of popular fabrics.
+    Its timeless design makes it one of our best sellers.
+    Contract Grade for unmatched durability.`,
+    dimensions: `Overall: 26"w x 31.6"d x 32.3"h.
+    Seat:
+    Width: 22.5"  Depth: 20.25"  Height: 19".
+    Arm height: 22.9".
+    Back height: 30.3".
+    Clearance: 8".
+    Diagonal depth: 29.5".
+    Packaged weight: 39.6 lbs.`,
+    details: `Solid ash wood arms and legs.
+    Engineered wood seat and back.
+    All wood is kiln-dried for extra durability.
+    Made with FSC®-certified wood =responsibly managed forests.
+    Webbed cushion support with foam filling.
+    Seat firmness: Medium. On a scale =1 to 5 (5 being firmest), it's a 4.
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential. See more.
+    Made in Romania.`,
+  },
+  {
+    id: 11,
+    name: "High Table",
+    image: [HighTablev1, HighTablev2, HighTablev3],
+    price: "$ 1,998",
+    about: `It's a substantial table with graceful lines.
+    Crafted =solid, sustainably sourced wood.
+    Contract Grade, so you know it's built to last.
+    You pick the finish & width.`,
+    dimensions: `Overall: 72"w x 39"d x 30"h.
+    Clearance under table: 29".
+    Distance between legs: 38.5".
+    Overhang: 13.5".
+    Top thickness: 1".
+    Comfortably seats up to: 6.`,
+    details: `Kiln-dried solid mango wood.
+    Natural variations in wood color and grain make each piece one of a kind.
+    The mango wood used on this product is sustainably sourced =trees that no longer produce fruit.
+    Legs include built-in levelers.
+    Made in a Fair Trade Certified™ factory, empowering workers who made it.
+    This contract-grade item is manufactured to meet the demands of commercial use in addition to residential. 
+    Made in India.
+    Patent pending.`,
+  },
+];
